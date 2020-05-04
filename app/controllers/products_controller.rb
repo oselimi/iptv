0200ad20cc5de4a91444a4255bf4c972ed8b62eb
oselimi <selimiosman94@gmail.com>
2020-05-04 11:11:49 +0200

    Update in Products Controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,7 +8,7 @@ class ProductsController < ApplicationController
 
   def show
     @products = Product.all
-    @product = Product.find(params[:id])
+    @product = Product.last
   end
 
   def new
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@ class Product < ApplicationRecord
   belongs_to :user
   validates :title,   presence: true, uniqueness: true
   validates :link,    presence: true, uniqueness: true
+  validates :user_id, presence: true
   has_one_attached :image
 end
